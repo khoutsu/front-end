673a88c6fd9acc3b469aca1108371298592396c8
khotsu <rutchanont_to66@live.rmutl.ac.th>
2024-02-02 15:43:16 +0700

    api fetch change

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,46 +1,41 @@
 export async function searchMovie(search_data) {
+  try {
+    console.log("search_data: " + search_data);
 
-    try{
+    const response = await fetch(
+      "http://api.se-rmutl.net/api/movie/search?search_text=" + search_data
+    );
 
-        console.log('search_data: ' + search_data);
+    return await response.json(); //***
 
-        const response = await fetch('http://localhost:4000/api/movie/search?search_text='+search_data);
-
-        return await response.json(); //***
-
-       // console.log('Search Movie response: ' + await response);
-
-    }catch(error) {
-        return [];
-    }
-    
+    // console.log('Search Movie response: ' + await response);
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function getAllMovies() {
+  try {
+    //const response = await fetch('/api/users');
+    //const response = await fetch('/api/movie/all');
+    const response = await fetch("http://api.se-rmutl.net/api/movie/all");
 
-    try{
-        //const response = await fetch('/api/users'); 
-        //const response = await fetch('/api/movie/all');
-        const response = await fetch('http://localhost:4000/api/movie/all');
-
-        //const response = await fetch('/api/movie/all');
-        return await response.json();
-    }catch(error) {
-        return [];
-    }
-    
+    //const response = await fetch('/api/movie/all');
+    return await response.json();
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function createMovie(data) {
-    const response = await fetch(`http://localhost:4000/api/movie/insert`, {
-        //mode: 'no-cors',
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
+  const response = await fetch(`http://api.se-rmutl.netapi/movie/insert`, {
+    //mode: 'no-cors',
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  console.log("Create Movie response: " + (await response));
 
-      })
-      console.log('Create Movie response: ' + await response);
-
-   // return  response.json();
-    return  response;
+  // return  response.json();
+  return response;
 }
